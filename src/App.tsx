87af3ca1ef4e1a1ecba9b89d16dda66116e561a8
mateusz.kowalski <mateusz.kowalski@trustmate.io>
2021-12-28 14:00:52 +0100

    basic structure of project

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useRef, useState } from 'react';
 import './App.css';
 
+const canvasX = 1000;
+const canvasY = 1000;
+const initialSnake = [[4,10], [4,10]];
+const initialApple = [14, 10];
+const scale = 50;
+const timeDelay = 100;
+
+
 function App() {
+  const canvasRef = useRef(null);
+  const [snake, setSnake] = useState(initialSnake);
+  const [apple, setApple] = useState(initialApple);
+  const [direction, setDirection] = useState([0, -1]);
+  const [snake, setSnake] = useState(initialSnake);
+  const [snake, setSnake] = useState(initialSnake);
+  const [snake, setSnake] = useState(initialSnake);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Test
     </div>
   );
 }
