b909ad2314f3e5c2a38e00ac971542c48a2a43d3
mateusz.kowalski <mateusz.kowalski@trustmate.io>
2021-12-30 08:23:08 +0100

    basic structure of project

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,11 @@
-import { useRef, useState } from 'react';
-import './App.css';
-
-const canvasX = 1000;
-const canvasY = 1000;
-const initialSnake = [[4,10], [4,10]];
-const initialApple = [14, 10];
-const scale = 50;
-const timeDelay = 100;
-
+import GlobalStyles from './GlobalStyle'
 
 function App() {
-  const canvasRef = useRef(null);
-  const [snake, setSnake] = useState(initialSnake);
-  const [apple, setApple] = useState(initialApple);
-  const [direction, setDirection] = useState([0, -1]);
-  const [snake, setSnake] = useState(initialSnake);
-  const [snake, setSnake] = useState(initialSnake);
-  const [snake, setSnake] = useState(initialSnake);
   return (
     <div>
-      Test
+      <GlobalStyles />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
